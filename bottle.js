88332a9fc6a9fe46c3e3de7e370d0f855648508e
Sana1408 <77221565+Sana1408@users.noreply.github.com>
2022-02-02 21:18:33 +0530

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,30 @@
+img = "";
+Status = "";
+
+function preload(){
+    img = loadImage("bottle.jpg");
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Object Detecting";
+
+    function modelLoaded(){
+        console.log("ModelLoaded!");
+        Status = true;
+        objectDetector.detect(img, gotResult);
+    }
+    
+    function gotResult(error, results){
+       if(error){
+            console.error(error);
+       }
+       console.log(results);
+    }
+}
